51af3ce456fdc1134900a71b3710b31b404c51f1
alitemel89 <64444827+alitemel89@users.noreply.github.com>
2021-03-10 15:43:09 +0300

    update MapQuest styles

--- src/components/MapQuest/MapQuestStyles.js
+++ src/components/MapQuest/MapQuestStyles.js
@@ -9,9 +9,14 @@ export const MapContainer = styled.div`
   box-shadow: 0px 1rem 2rem 0px rgba(0,0,0,0.75);
 
   @media screen and (max-width: 426px) {
-        width: 40rem;
+        width: auto;
         height: auto;
         font-size: 1rem;
+  }
+
+  @media screen and (max-width: 768px) {
+        width: auto;
+        height: auto;
   }
 
 `;
